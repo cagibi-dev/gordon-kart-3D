0cd194e7b4b66259691bb9b60a3d8018dd3f0a99
Paul BLANLOEIL <paul.blanloeil@etu.univ-nantes.fr>
2020-09-06 11:31:36 +0200

    Revamp day/night system

--- src/world/World.gd
+++ src/world/World.gd
@@ -1,4 +1,4 @@
-extends WorldEnvironment
+extends Spatial
 
 signal finished
 
@@ -6,6 +6,14 @@ var best_time := 999999.0
 var current_time := 0.0
 var running := false
 var can_finish := false
+
+func _ready():
+	var tod: OptionButton = $HUD/TopHud/Rows/TimeOfDay
+	tod.add_item("sunset", 0)
+	tod.add_item("dusk", 1)
+	tod.add_item("night", 2)
+	tod.add_item("day", 3)
+	_on_TimeOfDay_item_selected(0)
 
 func _input(event):
 	if event.is_action_pressed("ui_accept") and $Kart/StartCamera.current:
@@ -47,14 +55,12 @@ func _on_FinishLine_body_exited(_body):
 
 func _on_Midway_body_entered(_body):
 	can_finish = true
+	if not $Crowd.playing:
+		$Crowd.play()
 
 
 func _on_MusicOnOff_toggled(button_pressed):
 	AudioServer.set_bus_mute(1, not button_pressed)
-
-
-func _on_CrowdOnOff_toggled(button_pressed):
-	AudioServer.set_bus_mute(3, not button_pressed)
 
 
 func _on_LightOnOff_toggled(button_pressed):
@@ -65,10 +71,23 @@ func _on_AudioOnOff_toggled(button_pressed):
 	AudioServer.set_bus_mute(2, not button_pressed)
 
 
-func _on_Portal_body_entered(_body):
-	var err
-	if get_tree().current_scene.filename != "res://world/NightWorld.tscn":
-		err = get_tree().change_scene("res://world/NightWorld.tscn")
-	else:
-		err = get_tree().change_scene("res://world/World.tscn")
-	assert(err == OK)
+func _on_TimeOfDay_item_selected(index):
+	var cameras := [$Kart/StartCamera, $Kart/FirstPersonCamera, $Camera]
+	for c in cameras:
+		c.cull_mask &= ~30
+		c.cull_mask |= int(pow(2, index+1))
+		if index == 0:
+			c.environment = preload("res://env_sunset.tres")
+		if index == 1:
+			c.environment = preload("res://env_dusk.tres")
+		if index == 2:
+			c.environment = preload("res://env_night.tres")
+		if index == 3:
+			c.environment = preload("res://env_day.tres")
+	$Lights/SunLight.hide()
+	$Lights/DayLight.hide()
+	match index:
+		0: # sunset
+			$Lights/SunLight.show()
+		3: # day
+			$Lights/DayLight.show()
