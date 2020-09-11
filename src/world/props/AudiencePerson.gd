47f37a0cc172e07869db861dc83b564e1bd9dfc6
Paul BLANLOEIL <paul.blanloeil@etu.univ-nantes.fr>
2020-09-11 21:58:58 +0200

    Add audience diversity & correct smoke files

--- src/world/props/AudiencePerson.gd
+++ src/world/props/AudiencePerson.gd
@@ -1,6 +1,23 @@
 extends RigidBody
 
 
+const colors = [
+	preload("res://materials/DarkGreen.material"),
+	preload("res://materials/Grey.material"),
+	preload("res://materials/Green.material"),
+	preload("res://materials/Red.material"),
+	preload("res://materials/Yellow.material"),
+]
+
+func _ready():
+	# random cloth color
+	$Body.set_surface_material(0, colors[randi() % colors.size()])
+	if randi() % 2 == 0:
+		$Head/Hair.show()
+		var hair_mat = colors[randi() % colors.size()]
+		$Head/Hair.set_surface_material(0, hair_mat)
+		$Head/Hair/Hair2.set_surface_material(0, hair_mat)
+
 func _on_JumpTimer_timeout():
 	apply_central_impulse(Vector3.UP * 4)
 	$JumpTimer.wait_time = 1 + randf()
