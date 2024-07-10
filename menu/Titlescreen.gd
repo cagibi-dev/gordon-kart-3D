b8af6d8cfe3fdb2f3207fc567097f5387645a677
Paul BLANLOEIL <paul.blanloeil@etu.univ-nantes.fr>
2024-07-10 13:26:42 +0200

    Add Gordon Lake

--- menu/Titlescreen.gd
+++ menu/Titlescreen.gd
@@ -2,14 +2,37 @@ extends Node
 
 
 func _on_PlaySolo_pressed():
-	var err := get_tree().change_scene_to(preload("res://circuits/Lobby.tscn"))
-	assert(err == OK)
+	$MainPanel.hide()
+	go_to_lobby()
 
 
 func _on_PlayMulti_pressed():
-	pass # Replace with function body.
+	$Next.play()
+	create_tween().tween_property($MainPanel, "rotation", PI, 0.2)
+	$PlayOnline.show()
+	$PlayOnline.rotation = PI
+	create_tween().tween_property($PlayOnline, "rotation", 0, 0.2)
+	$PlayOnline/Username.grab_focus()
 
 
 func _on_MoreGames_pressed():
 	var err := OS.shell_open("https://cagibi.itch.io")
 	assert(err == OK)
+
+
+func _on_Host_pressed():
+	$PlayOnline.hide()
+	go_to_lobby()
+
+
+func _on_Join_pressed():
+	$PlayOnline.hide()
+	go_to_lobby()
+
+
+func go_to_lobby():
+	$Next.play()
+	yield($Next, "finished")
+	var err := get_tree().change_scene_to(preload("res://circuits/Lobby.tscn"))
+	assert(err == OK)
+
--- world/NoiseTerrain.gd
+++ world/NoiseTerrain.gd
@@ -1,14 +1,29 @@
+tool
 extends StaticBody
 
 
-export (Texture) var heightmap: Texture = preload("res://world/terrain_noise.tres")
-export (Vector2) var chunk_size := Vector2(32, 32)
+export (Texture) var heightmap: Texture setget set_heightmap
+export (Vector2) var chunk_size := Vector2(32, 32) setget set_size
 
 
-func _ready():
+func set_heightmap(new_hm: Texture):
+	heightmap = new_hm
+	recreate_terrain()
+
+
+func set_size(new_size: Vector2):
+	chunk_size = new_size
+	recreate_terrain()
+
+
+func recreate_terrain():
+	if not is_inside_tree():
+		return
 	var sea_level_offset: float = $Hills.translation.y
-	yield(heightmap, "changed")
+	#yield(heightmap, "changed")
 	var image := heightmap.get_data()
+	if image == null:
+		return
 	image.lock()
 
 	var map := PoolRealArray()
@@ -31,3 +46,8 @@ func _ready():
 	image.unlock()
 
 	$CollisionShape.shape.map_data = map
+
+	var mat: ShaderMaterial = $Hills.mesh.material
+	mat.set_shader_param("heightmap", heightmap)
+	mat = $Water.mesh.material
+	mat.set_shader_param("heightmap", heightmap)
